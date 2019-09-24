95753247872349e2c6ee929b1f28e623adedcd7b
happytm <36576353+happytm@users.noreply.github.com>
2019-09-24 12:48:23 -0400

    Update ESPCoreRulesSender.ino

--- ESPCoreRulesSender.ino
+++ ESPCoreRulesSender.ino
@@ -1,21 +1,21 @@
 /*
-This sketch uses IRremoteESP8266 library from https://github.com/crankyoldgit/IRremoteESP8266.
-In order to send MIDEA protocol IR message there is change needed in library
-as documented here : https://github.com/crankyoldgit/IRremoteESP8266/issues/887#issuecomment-531314986
+  This sketch uses IRremoteESP8266 library from https://github.com/crankyoldgit/IRremoteESP8266.
+  In order to send MIDEA protocol IR message there is change needed in library
+  as documented here : https://github.com/crankyoldgit/IRremoteESP8266/issues/887#issuecomment-531314986
 
-Change this line: https://github.com/crankyoldgit/IRremoteESP8266/blob/master/src/IRrecv.h#L402
+  Change this line: https://github.com/crankyoldgit/IRremoteESP8266/blob/master/src/IRrecv.h#L402
 
-to:
+  to:
 
                    bool strict = false);
-e.g.
+  e.g.
 
-#if DECODE_MIDEA
+  #if DECODE_MIDEA
   bool decodeMidea(decode_results *results, uint16_t nbits = kMideaBits,
                    bool strict = true);
-#endif
+  #endif
 
-On receiver side example can be used from here: https://github.com/happytm/BatteryNode/blob/master/Double%20Receiver/Double_Receiver.ino
+  On receiver side example can be used from here: https://github.com/happytm/BatteryNode/blob/master/Double%20Receiver/Double_Receiver.ino
 */
 #define IRSENDER              false
 #define PROBEREQUESTER        true
@@ -28,7 +28,7 @@ int device = 1;
 
 //============Do not need user configuration from here on============================
 
- 
+
 #if IRSENDER
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
@@ -37,16 +37,16 @@ int device = 1;
 //#include <IRutils.h>
 #endif
 
-//ADC_MODE(ADC_VCC); //vcc read-mode
-
+ADC_MODE(ADC_VCC); //vcc read-mode
+uint8 ssid[32] = "ESP_Controller";
 int apChannel = 7;
 int temperature = 10;
 int humidity = 20;
 int pressure = 90;
 int voltage = 80;
-int light = 70; 
+int light = 70;
 
-uint8_t irMac[] = {temperature, humidity, pressure, voltage, light, device}; 
+uint8_t irMac[] = {temperature, humidity, pressure, voltage, light, device};
 
 int VOLT_LIMIT = 3;
 unsigned long lastMillis;
@@ -61,63 +61,64 @@ const uint16_t kIrLed = 16;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 
 
-//uint64_t myMac = 0x1234567890AB;//{temperature, humidity, pressure, battery, lux, device};  // 48bits 
-uint64_t myMac = 
-    (uint64_t)(uint8_t (irMac[5])) | 
-    (uint64_t)(uint8_t (irMac[4])) << 8 |          
-    (uint64_t)(uint8_t (irMac[3])) << 16 |          
-    (uint64_t)(uint8_t (irMac[2])) << 24 |          
-    (uint64_t)(uint8_t (irMac[1])) << 32 |          
-    (uint64_t)(uint8_t (irMac[0])) << 40;    
+//uint64_t myMac = 0x1234567890AB;//{temperature, humidity, pressure, battery, lux, device};  // 48bits
+uint64_t myMac =
+  (uint64_t)(uint8_t (irMac[5])) |
+  (uint64_t)(uint8_t (irMac[4])) << 8 |
+  (uint64_t)(uint8_t (irMac[3])) << 16 |
+  (uint64_t)(uint8_t (irMac[2])) << 24 |
+  (uint64_t)(uint8_t (irMac[1])) << 32 |
+  (uint64_t)(uint8_t (irMac[0])) << 40;
 #endif
-    
-   
+
+
 //====================IR defines ends==============================
 
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println();
-    Serial.print("Milliseconds passed before setup: ");
-    Serial.println(millis());
-    lastMillis = millis();
-    Serial.println();
-    
-    //#if PROBEREQUESTER
-    wifi_set_macaddr(STATION_IF, irMac);
-    WiFi.disconnect();
-    WiFi.hostname("Livingroom");
-    Serial.println();
-    //#endif
-  /*  
-    float voltage = ESP.getVcc() / (float)1023; // * (float)1.07;
-    Serial.print("Voltage: "); Serial.print(voltage); Serial.print("  Voltage Expected: "); Serial.println(VOLT_LIMIT);
-    if (voltage < VOLT_LIMIT)      // if voltage of battery gets to low, the LED wil blink fast.
+
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Milliseconds passed before setup: ");
+  Serial.println(millis());
+  lastMillis = millis();
+  Serial.println();
+  delay(1);
+
+#if PROBEREQUESTER
+  wifi_set_macaddr(STATION_IF, irMac);
+  WiFi.disconnect();
+  WiFi.hostname("Livingroom");
+  Serial.println();
+#endif
+
+  float voltage = ESP.getVcc() / (float)1023; // * (float)1.07;
+  Serial.print("Voltage: "); Serial.print(voltage); Serial.print("  Voltage Expected: "); Serial.println(VOLT_LIMIT);
+  if (voltage < VOLT_LIMIT)      // if voltage of battery gets to low, the LED wil blink fast.
   {
     Serial.println("Warning :- Battery Voltage low please change batteries" );
     Serial.println();
   }
-    */
-    Serial.println("Setup finished");
-    
-    
-    passedMillis = millis() - lastMillis;
-    Serial.print("Time spent on setup: "); 
-    Serial.println(passedMillis);
-    Serial.println();
-    lastMillis = millis();
 
-   
-    Serial.println();
-    
-    #if PROBEREQUESTER
-    probeRequest();
-    #endif
-    
-    #if IRSENDER
-    irsend.begin();
-    irSender();
-    #endif
+  Serial.println("Setup finished");
+
+  //#if PROBEREQUESTER
+  probeRequest();
+  //#endif
+
+#if IRSENDER
+  irsend.begin();
+  irSender();
+#endif
+
+
+  passedMillis = millis() - lastMillis;
+  Serial.print("Time spent on setup: ");
+  Serial.println(passedMillis);
+  Serial.println();
+  lastMillis = millis();
+  Serial.println();
 
 }   // Setup ends here
 
@@ -125,27 +126,27 @@ void setup() {
 
 void loop() {
 
-//===========================IR loop===============================
-  #if IRSENDER
+  //===========================IR loop===============================
+#if IRSENDER
   //irSender();
-  #endif
- //=====================Probe request Loop=========================
-  
-// put your code here to run in loop or move to setup to run oncce.
- 
- #if PROBEREQUESTER
- probeRequest();
- #endif
- 
- Serial.print("Total time I spent before going to sleep: "); 
- Serial.println(millis());
- Serial.print("I will wakeup in: "); 
- Serial.print(SLEEP_SECS / 60); 
- Serial.println(" Minutes"); 
- delay(20000);
- ESP.restart(); 
-//ESP.deepSleep(0);
- 
+#endif
+  //=====================Probe request Loop=========================
+
+  // put your code here to run in loop or move to setup to run oncce.
+
+#if PROBEREQUESTER
+  probeRequest();
+#endif
+
+  Serial.print("Total time I spent before going to sleep: ");
+  Serial.println(millis());
+  Serial.print("I will wakeup in: ");
+  Serial.print(SLEEP_SECS / 60);
+  Serial.println(" Minutes");
+  delay(20000);
+  ESP.restart();
+  //ESP.deepSleep(0);
+
 }
 
 //=========================Main Loop ends==========================
@@ -156,82 +157,87 @@ void gotoSleep() {                            //need connection between GPIO16 a
   int sleepSecs = SLEEP_SECS;// + ((uint8_t)RANDOM_REG32/2);
   Serial.printf("Up for %i ms, going to sleep for %i secs...\n", millis(), sleepSecs);
   ESP.deepSleep(sleepSecs * 1000000, RF_NO_CAL);
+  ////ESP.deepSleep( SLEEPTIME, WAKE_RF_DISABLED );
 }
 
 #if PROBEREQUESTER
 //=========================Probe request function starts===========
-  
+
 void probeRequest()  {
   Serial.println("Starting Probe sender");
   Serial.println("Sending sensor data over Probe request protocol to Master Node");
   Serial.println();
- 
-//int8_t scanNetworks(bool async = true, bool show_hidden = false, uint8 channel = 0, uint8* ssid = NULL);
-int n = WiFi.scanNetworks(true, true, apChannel);  // find solution for scan for specific ssid instead of channel.
+
+
+
+  //int8_t scanNetworks(bool async = true, bool show_hidden = false, uint8 channel = 0, uint8* ssid = NULL);
+  int n = WiFi.scanNetworks(true, false, apChannel, (uint8*) ssid);
 
 #if !IRSENDER
-delay(25); // Minimum delay required to scan networks in async mode if not using IRSENDER 
+  delay(25); // Minimum delay of 25 required to scan networks in async mode if not using IRSENDER
 #endif
 
 #if IRSENDER
-delay(10);     // Minimum delay required to scan networks in async mode if using IRSENDER              
+  delay(10);     // Minimum delay of 10 required to scan networks in async mode if using IRSENDER
 #endif
 
- if (WiFi.BSSIDstr(0)[16] == '1')  {     //use device number here
- String ssid;
- Serial.println();
- Serial.print("This Device MAC ID is: ");
- Serial.println(WiFi.macAddress());
- Serial.print("This Device Name is: ");
- Serial.println(WiFi.hostname());
- Serial.print("Message received from Controller is: ");
- Serial.println(&WiFi.BSSIDstr(0)[0]);
- Serial.println(WiFi.BSSIDstr(0)[0]);
- Serial.println(WiFi.BSSIDstr(0)[1]);
- Serial.println(WiFi.BSSIDstr(0)[2]);
- Serial.println(WiFi.BSSIDstr(0)[3]);
- Serial.println(WiFi.BSSIDstr(0)[4]);
- Serial.println(WiFi.BSSIDstr(0)[5]);
- Serial.println(WiFi.BSSIDstr(0)[6]);
- Serial.println(WiFi.BSSIDstr(0)[7]);
- Serial.println(WiFi.BSSIDstr(0)[8]);
- Serial.println(WiFi.BSSIDstr(0)[9]);
- Serial.println(WiFi.BSSIDstr(0)[10]);
- Serial.println(WiFi.BSSIDstr(0)[11]);
- Serial.println(WiFi.BSSIDstr(0)[12]);
- Serial.println(WiFi.BSSIDstr(0)[13]);
- Serial.println(WiFi.BSSIDstr(0)[14]);
- Serial.println(WiFi.BSSIDstr(0)[15]);
- Serial.println(WiFi.BSSIDstr(0)[16]);
- 
- Serial.print("Gateway Name is: ");
- Serial.println(WiFi.SSID(0));
- Serial.print("Gateway Channel is: ");
- Serial.println(WiFi.channel(0));
- 
- Serial.print("Message received from Controller: ");
- if (WiFi.BSSIDstr(0)[0] == '3')  {
-       Serial.print("GPIO,");
-       Serial.print(ledPin);
-       Serial.print(",");
-       Serial.print(WiFi.BSSIDstr(0)[15]);
+  if (WiFi.BSSIDstr(0)[16] == '1')  {     //use device number here
+    String ssid;
+    Serial.println();
+    Serial.print("This Device MAC ID is: ");
+    Serial.println(WiFi.macAddress());
+    Serial.print("This Device Name is: ");
+    Serial.println(WiFi.hostname());
+    Serial.print("Message received from Controller is: ");
+    Serial.println(&WiFi.BSSIDstr(0)[0]);
+    /* Serial.print(WiFi.BSSIDstr(0)[0]);
+      Serial.print(WiFi.BSSIDstr(0)[1]);
+      Serial.println(WiFi.BSSIDstr(0)[2]);
+      Serial.print(WiFi.BSSIDstr(0)[3]);
+      Serial.print(WiFi.BSSIDstr(0)[4]);
+      Serial.println(WiFi.BSSIDstr(0)[5]);
+      Serial.print(WiFi.BSSIDstr(0)[6]);
+      Serial.print(WiFi.BSSIDstr(0)[7]);
+      Serial.println(WiFi.BSSIDstr(0)[8]);
+      Serial.print(WiFi.BSSIDstr(0)[9]);
+      Serial.print(WiFi.BSSIDstr(0)[10]);
+      Serial.println(WiFi.BSSIDstr(0)[11]);
+      Serial.print(WiFi.BSSIDstr(0)[12]);
+      Serial.print(WiFi.BSSIDstr(0)[13]);
+      Serial.println(WiFi.BSSIDstr(0)[14]);
+      Serial.print(WiFi.BSSIDstr(0)[15]);
+      Serial.print(WiFi.BSSIDstr(0)[16]);
+    */
+    Serial.println();
+
+    Serial.print("Gateway Name is: ");
+    Serial.println(WiFi.SSID(0));
+    Serial.print("Gateway Channel is: ");
+    Serial.println(WiFi.channel(0));
+
+    Serial.print("Message received from Controller: ");
+    if (WiFi.BSSIDstr(0)[0] == '3')  {
+      Serial.print("GPIO,");
+      Serial.print(ledPin);
+      Serial.print(",");
+      Serial.print(WiFi.BSSIDstr(0)[15]);
     }
 
     if (WiFi.BSSIDstr(0)[1] == '1')  {
-       Serial.print("I will wake up next time with WiFi & OTA and deepsleep disabled");
-       // Define some features here to activate wifi & disable deepsleep when unit wakes up next time.
+      Serial.print("I will wake up next time with WiFi & OTA and deepsleep disabled");
+      // Define some features here to activate wifi & set 5 seconds deep sleep time & disable deepsleep when unit wakes up next time.
     }
- }
+  }
 
- Serial.println();
- WiFi.scanDelete();
- passedMillis = millis() - lastMillis;
- Serial.print("Time spent on Probe Request: "); 
- Serial.println(passedMillis);
- lastMillis = millis();
- Serial.println();
- 
- delay(10);
+  Serial.println();
+  WiFi.scanDelete();
+  passedMillis = millis() - lastMillis;
+  Serial.print("Time spent on Probe Request: ");
+  Serial.println(passedMillis);
+  lastMillis = millis();
+  Serial.println();
+
+  delay(10);    // minimum delay of 10 required here.
 
 }
 
@@ -240,22 +246,22 @@ delay(10);     // Minimum delay required to scan networks in async mode if using
 #if IRSENDER
 //========================IR function starts=========================================
 void irSender()   {
-    
+
   Serial.println("Starting IR data sender");
   Serial.println("Sending sensor data over IR protocol to Master Node");
   Serial.println();
-   
-   irsend.sendMidea(myMac); 
-   Serial.print("Message sent : ");
-//   serialPrintUint64(myMac, HEX); //  
-   Serial.println();
-   
- 
-    passedMillis = millis() - lastMillis;
-    Serial.print("Time spent on IR Sender: "); 
-    Serial.println(passedMillis);
-    lastMillis = millis();
-    Serial.println();
-    //delay(10);
+
+  irsend.sendMidea(myMac);
+  Serial.print("Message sent : ");
+  //   serialPrintUint64(myMac, HEX); //
+  Serial.println();
+
+
+  passedMillis = millis() - lastMillis;
+  Serial.print("Time spent on IR Sender: ");
+  Serial.println(passedMillis);
+  lastMillis = millis();
+  Serial.println();
+  //delay(10);
 }
 #endif
